14db463b214e85de4cb4d1e0efc008045f54e9e5
Alkader-A <tripinfo040@gmail.com>
2023-04-25 11:38:55 -0700

    main.h

--- main.h
+++ main.h
@@ -1,4 +1,3 @@
-#ifndef MAIN_H
 #define MAIN_H
 #include <stdarg.h>
 #include <stdio.h>
@@ -113,5 +112,3 @@ int is_digit(char);
 
 long int convert_size_number(long int num, int size);
 long int convert_size_unsgnd(unsigned long int num, int size);
-
-#endif /* MAIN_H */
